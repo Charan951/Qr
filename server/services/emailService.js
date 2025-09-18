70eeb6826ead7424dae8b28a60e241d6c2e9f610
Samyou02 <samyuktha@speshway.com>
2025-09-18 22:20:33 +0530

    hi

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -7,6 +7,7 @@ const createTransporter = () => {
     port: process.env.SMTP_PORT,
     secure: false, // true for 465, false for other ports
     auth: {
+      
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
